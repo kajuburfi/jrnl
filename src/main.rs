f6cdf1df16e792479b1e2b25f2700261a6e26915
python128 <nexinov5vatr@gmail.com>
2025-03-25 11:19:37 +0530

    Add Chrono; Basic datetime and checking if file exists or not, making the file;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// Author: Tejas Gudgunti
+use chrono::Datelike;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let today = chrono::Local::now(); //Get `now` time
+    let today_date = today.format("%Y-%m-%d");
+    println!("{}", today_date);
+
+    let filename = format!(
+        "jrnl/{}/{}_{}.md",
+        today.year(),
+        today.year(),
+        today.month()
+    );
+    check_file_exist(filename);
+}
+
+/// Checks if the file exists, if not, it makes the file.
+fn check_file_exist(filename: String) {
+    let path = Path::new(&filename);
+    println!("{:#?}; {}", path, path.exists());
+
+    if !path.exists() {
+        let data_file = File::create_new(filename).expect("Could not make new file.");
+        println!("Made file {:?}", data_file);
+    }
 }
